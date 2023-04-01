ebd8250b4982e77e3a5629d4911b82946a353806
Tobias Happ <tobias.happ@gmx.de>
2023-04-01 21:20:34 +0200

    feat: add dummy pages for datenschutz and impressum

---
+++ src/routes/impressum/+page.svelte
@@ -0,0 +1,8 @@
+<script type="ts">
+    import Section from '$lib/components/Section.svelte';
+</script>
+
+<Section title="Impressum">
+    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Obcaecati, consectetur nisi dolorum esse sit libero
+    numquam enim sequi repellat ducimus commodi aliquam aliquid recusandae aperiam sunt ipsa earum pariatur error.
+</Section>
