19a5bba02c65180860f29ce82ddf1793afa47af4
Tobias Happ <tobias.happ@gmx.de>
2023-04-01 14:53:00 +0200

    feat: add text sections

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,6 @@
 <script type="ts">
 	import imageHeader from '$lib/assets/header.png';
+	import Section from './Section.svelte';
 </script>
 
 <header
@@ -17,3 +18,35 @@
 		popCHORköln
 	</h1>
 </header>
+
+<Section title="Chor">
+	<p>
+		Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit dolor nihil unde, natus reiciendis possimus velit
+		magni voluptatum reprehenderit iure, similique odio ullam maxime corrupti qui itaque ad, repellendus perspiciatis
+		exercitationem. Labore facilis voluptas numquam aliquid quibusdam possimus odit eos explicabo vitae, odio dolor
+		doloremque fuga eveniet consequatur! Dolore soluta cupiditate asperiores recusandae minima id quisquam corrupti
+		ratione deleniti dolorem libero magnam laudantium voluptate animi sit aliquid similique provident, maxime eius,
+		molestias error omnis excepturi quasi? Facilis accusantium ipsa soluta, perspiciatis saepe, eos repellendus aperiam
+		blanditiis inventore eveniet modi dolorem? Laboriosam nulla voluptate at odit, quos ipsum atque aut repudiandae sunt
+		impedit fuga corporis repellat suscipit blanditiis quod sint ipsam, accusamus quis laudantium rem modi, velit autem
+		totam? Sit eum neque maiores sequi expedita laborum libero officiis saepe totam dolore ratione voluptatibus, et vel
+		pariatur. Obcaecati commodi ipsa nam laudantium. Placeat ullam illum suscipit, dolore sit repudiandae id non
+		eveniet!
+	</p>
+</Section>
+
+<Section title="Chorleiter">
+	<p>
+		Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit dolor nihil unde, natus reiciendis possimus velit
+		magni voluptatum reprehenderit iure, similique odio ullam maxime corrupti qui itaque ad, repellendus perspiciatis
+		exercitationem. Labore facilis voluptas numquam aliquid quibusdam possimus odit eos explicabo vitae, odio dolor
+		doloremque fuga eveniet consequatur! Dolore soluta cupiditate asperiores recusandae minima id quisquam corrupti
+		ratione deleniti dolorem libero magnam laudantium voluptate animi sit aliquid similique provident, maxime eius,
+		molestias error omnis excepturi quasi? Facilis accusantium ipsa soluta, perspiciatis saepe, eos repellendus aperiam
+		blanditiis inventore eveniet modi dolorem? Laboriosam nulla voluptate at odit, quos ipsum atque aut repudiandae sunt
+		impedit fuga corporis repellat suscipit blanditiis quod sint ipsam, accusamus quis laudantium rem modi, velit autem
+		totam? Sit eum neque maiores sequi expedita laborum libero officiis saepe totam dolore ratione voluptatibus, et vel
+		pariatur. Obcaecati commodi ipsa nam laudantium. Placeat ullam illum suscipit, dolore sit repudiandae id non
+		eveniet!
+	</p>
+</Section>
---
+++ src/routes/Section.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+	export let title: string;
+</script>
+
+<section class="container mx-auto px-4 py-4">
+	<hgroup
+		class="
+			before:-z-1 relative my-4 flex justify-center
+			before:absolute before:inset-x-0 before:top-[50%] before:h-[1px] before:bg-slate-500
+		"
+	>
+		<h2 class="relative bg-white px-8 text-3xl font-medium first-letter:text-[#FF6666]">{title}</h2>
+	</hgroup>
+
+	<slot />
+</section>
