accb8df45022030462bca0184b0a33f2305196e6
Tobias Happ <tobias.happ@gmx.de>
2023-04-01 14:29:47 +0200

    feat: set font-family to Open Sans

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,4 +1,5 @@
-<script>
+<script type="ts">
+	import '@fontsource/open-sans';
 	import '../app.postcss';
 </script>
 
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,6 +2,9 @@ const config = {
 	content: ['./src/**/*.{html,js,svelte,ts}'],
 
 	theme: {
+		fontFamily: {
+			sans: ['Open Sans'],
+		},
 		extend: {},
 	},
 
