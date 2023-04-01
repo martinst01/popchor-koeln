37c6261d959434b3f4f006a7f9b4f79842ad2eee
Tobias Happ <tobias.happ@gmx.de>
2023-04-01 14:31:03 +0200

    feat: add simple header

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,19 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script type="ts">
+	import imageHeader from '$lib/assets/header.png';
+</script>
+
+<header
+	class="
+		mx-auto bg-[#BBBAB5] bg-[length:40rem] bg-bottom bg-no-repeat pb-16 pt-8
+		sm:bg-[length:50rem] sm:pb-20
+		md:bg-[length:60rem] md:pb-24
+		lg:bg-[length:80rem] lg:pb-32
+	"
+	style="background-image: url('{imageHeader}'"
+>
+	<h1 class="text-center text-4xl font-bold leading-[4rem]">
+		d'acCHORd
+		<small class="block text-2xl">heißt jetzt</small>
+		popCHORköln
+	</h1>
+</header>
