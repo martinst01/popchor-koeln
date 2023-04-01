220ef339b934e0d52a46c5ff8dedcee983bc541e
Tobias Happ <tobias.happ@gmx.de>
2023-04-01 21:15:30 +0200

    refactor: move header to layout

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,23 @@
 <script type="ts">
+    import imageHeader from '$lib/assets/header.png';
     import '@fontsource/open-sans';
     import '../app.postcss';
 </script>
+
+<header
+    class="
+		mx-auto bg-[#BBBAB5] bg-[length:40rem] bg-bottom bg-no-repeat pb-16 pt-8
+		sm:bg-[length:50rem] sm:pb-20
+		md:bg-[length:60rem] md:pb-24
+		lg:bg-[length:80rem] lg:pb-32
+	"
+    style="background-image: url('{imageHeader}'"
+>
+    <h1 class="text-center text-4xl font-bold leading-[4rem]">
+        d'acCHORd
+        <small class="block text-2xl">heißt jetzt</small>
+        popCHORköln
+    </h1>
+</header>
 
 <slot />
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,4 @@
 <script type="ts">
-    import imageHeader from '$lib/assets/header.png';
     import Contact from './Contact.svelte';
     import Gig from './Gig.svelte';
     import Section from './Section.svelte';
@@ -59,22 +58,6 @@
         },
     ];
 </script>
-
-<header
-    class="
-		mx-auto bg-[#BBBAB5] bg-[length:40rem] bg-bottom bg-no-repeat pb-16 pt-8
-		sm:bg-[length:50rem] sm:pb-20
-		md:bg-[length:60rem] md:pb-24
-		lg:bg-[length:80rem] lg:pb-32
-	"
-    style="background-image: url('{imageHeader}'"
->
-    <h1 class="text-center text-4xl font-bold leading-[4rem]">
-        d'acCHORd
-        <small class="block text-2xl">heißt jetzt</small>
-        popCHORköln
-    </h1>
-</header>
 
 <Section title="Chor">
     <p>
