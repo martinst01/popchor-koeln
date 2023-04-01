90f9e60de817fc273a989e6c679a649bdd01c5e3
Tobias Happ <tobias.happ@gmx.de>
2023-04-01 22:51:36 +0200

    fix: use lang attribute at scripts tags

--- src/routes/(index)/+page.svelte
+++ src/routes/(index)/+page.svelte
@@ -1,4 +1,4 @@
-<script type="ts">
+<script lang="ts">
     import Section from '$lib/components/Section.svelte';
     import Contact from './Contact.svelte';
     import Gig from './Gig.svelte';
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,4 +1,4 @@
-<script type="ts">
+<script lang="ts">
     import imageHeader from '$lib/assets/header.png';
     import '@fontsource/open-sans';
     import { Facebook, Instagram, Mail, Youtube } from 'lucide-svelte';
