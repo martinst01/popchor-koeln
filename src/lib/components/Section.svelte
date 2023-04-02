be95e551682ae9830834301bba17c9b3558b25f8
Tobias Happ <tobias.happ@gmx.de>
2023-04-02 15:40:42 +0200

    feat: indent content of sections

--- src/lib/components/Section.svelte
+++ src/lib/components/Section.svelte
@@ -6,12 +6,14 @@
 <section {id} class="container mx-auto my-8 p-4">
     <hgroup
         class="
-            before:-z-1 relative mb-4 flex justify-center
+            before:-z-1 relative mb-6 flex justify-center
             before:absolute before:inset-x-0 before:top-1/2 before:h-px before:bg-slate-500
         "
     >
         <h2 class="relative bg-white px-8 text-3xl font-medium first-letter:text-accent">{title}</h2>
     </hgroup>
 
-    <slot />
+    <div class="px-4 sm:px-8 lg:px-16">
+        <slot />
+    </div>
 </section>
