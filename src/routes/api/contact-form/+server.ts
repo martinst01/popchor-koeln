7cfb9685b8f5a04a77979beba8ce0d28180ed3bb
Tobias Happ <tobias.happ@gmx.de>
2023-04-04 14:33:44 +0200

    feat: implement contact-form endpoint

--- src/routes/api/contact-form/+server.ts
+++ src/routes/api/contact-form/+server.ts
@@ -1,4 +1,12 @@
+import {
+    CONTACT_FORM_RECEIVER_MAIL,
+    CONTACT_FORM_RECEIVER_NAME,
+    CONTACT_FORM_SENDER_MAIL,
+    CONTACT_FORM_SENDER_NAME,
+} from '$env/static/private';
 import { parseRequestBody } from '$lib/api';
+import { sendMail } from '$lib/mail';
+import { addContactToAudience } from '$lib/mailchimp';
 import { json } from '@sveltejs/kit';
 import { z } from 'zod';
 import type { RequestHandler } from './$types';
@@ -12,10 +20,34 @@ const RequestBodySchema = z.object({
 });
 export type RequestBody = z.infer<typeof RequestBodySchema>;
 
+const handleNewsletterCreation = async (newsletter: boolean, email: string) => {
+    if (!newsletter) {
+        return 'nicht abonniert';
+    }
+
+    const success = await addContactToAudience(email);
+
+    return success ? 'abbonniert' : 'Abonnieren fehlgeschlagen';
+};
+
 export const POST: RequestHandler = async ({ request }) => {
     return await parseRequestBody(request, RequestBodySchema, async (body) => {
-        console.log(body);
+        const newsletterResult = await handleNewsletterCreation(body.newsletter, body.email);
 
-        return json({ success: true });
+        const emailText = `Name: ${body.name}\nNewsletter: ${newsletterResult}\n\n${body.message}`;
+
+        const success = await sendMail({
+            from: { name: CONTACT_FORM_SENDER_NAME, address: CONTACT_FORM_SENDER_MAIL },
+            to: { name: CONTACT_FORM_RECEIVER_NAME, address: CONTACT_FORM_RECEIVER_MAIL },
+            replyTo: { name: body.name, address: body.email },
+            subject: `Kontaktformular: ${body.subject}`,
+            text: emailText,
+        });
+
+        if (success) {
+            return json({ success: true });
+        }
+
+        return json({ success: false }, { status: 500 });
     });
 };
