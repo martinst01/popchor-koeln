5d9520dbd9981802a0de87d8d392d2be5a844f4d
Tobias Happ <tobias.happ@gmx.de>
2023-04-04 23:54:46 +0200

    refactor: split header into separate component

---
+++ src/routes/Header.svelte
@@ -0,0 +1,33 @@
+<script lang="ts">
+    import imageHeader160 from '$lib/assets/header-160px.png';
+    import imageHeader240 from '$lib/assets/header-240px.png';
+    import imageHeader320 from '$lib/assets/header-320px.png';
+    import { Menu } from 'lucide-svelte';
+
+    export let toggleNavigation: () => void;
+</script>
+
+<header>
+    <div class="container relative mx-auto p-8 pb-4">
+        <a href="/">
+            <h1 class="text-center text-4xl font-bold leading-[4rem]">
+                d'acCHORd
+                <small class="block text-2xl">heißt jetzt</small>
+                popCHORköln
+            </h1>
+        </a>
+
+        <button class="absolute right-6 top-6 p-2" on:click={toggleNavigation}>
+            <Menu class="text-accent" size={32} />
+        </button>
+    </div>
+
+    <div class="flex h-[160px] justify-center sm:h-[240px] md:h-[320px]">
+        <picture>
+            <source media="(max-width: 639px)" srcset="{imageHeader160} 1x, {imageHeader320} 2x" />
+            <source media="(min-width: 640px) and (max-width: 767px)" srcset={imageHeader240} />
+            <source media="(min-width: 768px)" srcset={imageHeader320} />
+            <img src={imageHeader320} alt="Header Bild von dem gesamten Chor" class="h-full object-cover" />
+        </picture>
+    </div>
+</header>
